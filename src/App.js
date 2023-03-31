51e75212c60b7655d7f4b7ceae9226404407b4a0
unknown <sarojsardar25@gmail.com>
2023-03-31 07:56:54 +0545

    use State

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 
+import { useState } from 'react';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 
 function App() {
+  const [text, setText] = useState('This is text varible');
+  const [count, setCount] = useState(10);
   return (
     <div className="App">
      <h1>Welcome To React</h1>
-      <button>Simple Button</button>
         <Button as="a" variant="success">
-            Button as link
+            Hello React
         </Button>
+        <h4>{text}</h4>
+        <p>You clicked {count}</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click Me
+        </button>
     </div>
   );
 }
