88615c4bff890e5f571d6d37862c61b2e3bb3ab1
unknown <sarojsardar25@gmail.com>
2023-03-31 08:51:03 +0545

    Data Fetched

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 function App() {
-  const [text, setText] = useState('This is text varible');
+  const [text] = useState('This is text varible');
   const [count, setCount] = useState(10);
+
+  const [quote, setQuote] = useState([]);
+
+  const fetchData = () => {
+    return axios.get("https://dummyjson.com/quotes")
+    .then((response) => setQuote(response.data['quotes']));
+  }
+
+  useEffect(() => {
+     fetchData();
+  },[])
+
   return (
     <div className="App">
      <h1>Welcome To React</h1>
@@ -14,9 +27,17 @@ function App() {
         </Button>
         <h4>{text}</h4>
         <p>You clicked {count}</p>
-        <button onClick={() => setCount(count + 1)}>
+        <button onClick={() => setCount(count + 5)}>
           Click Me
         </button>
+
+        <h3>Quotes List</h3>
+        <ul>
+          {quote && quote.length>0 && quote.map((quoteObj, index) =>(
+           <li key={quoteObj.id}>Quote : {quoteObj.quote} - Author : {quoteObj.author} </li>
+          )) }
+        </ul>
+
     </div>
   );
 }
